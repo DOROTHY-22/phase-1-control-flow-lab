7b6dc3f35809c19b2486fc1eebd031606be3d997
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-03-08 19:00:11 +0300

    Make changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function scuberGreetingForFeet(distance){
+/*function scuberGreetingForFeet(distance){
   if (distance <= 400){
     return 'This one is on me!';
   }  
@@ -17,8 +17,9 @@ function scuberGreetingForFeet(distance){
 
 function ternaryCheckCity(city){
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-  // Write your code here!
+  
 }
+
 
 function switchOnCharmFromTip(tip){
   switch (tip){
@@ -28,6 +29,49 @@ function switchOnCharmFromTip(tip){
   return 'Thank you.';
   default:
     return 'Bye.';
+}}*/
+function scuberGreetingForFeet(distance){
+  if (distance <= 400 ){
+    return`This one is on me!`;
+  }
+  else if (distance > 400 && distance<= 2000 ){
+    return`That will be twenty bucks.`;
+  }
+  else if (distance > 2000 && distance<=2500 ){
+    return `I will gladly take your thirty bucks.`;
+  }
+  else {
+    return `No can do.`;
   }}
+  function ternaryCheckCity(city){
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  
+  } 
+ function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+      default:
+        return 'Bye.'
+  }
+  }
+  
+    
+    
+  
+  
+    
+
+  
+  
+
+
+
+
+
+
+
 
   // Write your code here!
